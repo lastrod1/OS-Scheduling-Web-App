9cdf0f2ed40c84a06ac742f728c02d210a00a610
lastrod1 <mtran3938@gmail.com>
2025-02-23 01:10:42 -0600

    Added function for generating random processes

---
+++ src/utilities/processGenerator.ts
@@ -0,0 +1,21 @@
+import { Process } from "../types/process";
+
+//this makes a random number of processes with random features
+//returns an array of processes to be processed by the scheduling algorithms
+
+export function generateProcesses(count:number) : Process[]
+{
+    let MaxBurst = 7;
+    let MaxArrival = 7;
+
+    let processes: Process[] = [];
+
+    for(let i = 0; i < count; i++)
+    {
+        processes.push({pid: i+1, 
+                        arrivalTime: Math.floor(Math.random() * MaxArrival), 
+                        burstTime: Math.floor(Math.random()* MaxBurst)});
+    }
+    return processes
+}
+
